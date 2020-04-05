00e78eb9e825b3612b5447c796cc6d95abdb8f0d
Shaun Walker <shaun.walker@siliqon.com>
2020-04-05 15:57:09 -0400

    upgrade to latest Oqtane framework

---
+++ ThemeInfo.cs
@@ -0,0 +1,13 @@
+﻿using Oqtane.Themes;
+
+namespace Oqtane.Theme.Custom
+{
+    public class ThemeInfo : ITheme
+    {
+        public Models.Theme Theme => new Models.Theme
+        {
+            Name = "Oqtane Theme",
+            Version = "1.0.0"
+        };
+    }
+}
